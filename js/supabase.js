595a432c2927867ac52868a54ef9c9179a714f02
AlexeyBoldin <aleksejboldin@gmail.com>
2026-07-20 09:41:27 +0300

    Change Supabase key to new publishable key
    Updated Supabase key to a new publishable key.

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://ofwmifxoqpapabbygujo.supabase.co";
 
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9md21pZnhvcXBhcGFiYnlndWpvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ0OTk4MDQsImV4cCI6MjEwMDA";
+const SUPABASE_KEY = "sb_publishable_TeE8cEYd4hTc6_NHU_luJw_oAZAErFM";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
