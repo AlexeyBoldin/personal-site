8db065cf84e23cd49cbf1fb9badc3dce5e28b8a0
AlexeyBoldin <aleksejboldin@gmail.com>
2026-07-20 11:00:19 +0300

    Add email field to order insertion

--- js/app.js
+++ js/app.js
@@ -440,6 +440,7 @@ orderFormEl.addEventListener("submit", async function (e) {
 				{
 					order_number: number,
 					name: nameInput.value.trim(),
+					email: emailInput.value.trim(),
 					phone: phoneInput.value.trim(),
 					address: addressInput.value.trim(),
 					quantity: Number(qtyInput.value),
