a9598dba48c9b9b6735b0df1ea2a26688651157b
AlexeyBoldin <aleksejboldin@gmail.com>
2026-07-24 13:26:22 +0300

    Add files via upload

---
+++ js/supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://ofwmifxoqpapabbygujo.supabase.co";
+
+const SUPABASE_KEY = "ВСТАВЬ_СЮДА_СВОЙ_ANON_KEY";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
