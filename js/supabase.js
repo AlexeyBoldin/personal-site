cc4bb38950e2596682c93a66d2a7c731a2d7cd92
AlexeyBoldin <aleksejboldin@gmail.com>
2026-07-24 13:20:18 +0300

    Delete js/supabase.js

--- js/supabase.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_URL = "https://ofwmifxoqpapabbygujo.supabase.co";
-
-const SUPABASE_KEY = "sb_publishable_TeE8cEYd4hTc6_NHU_luJw_oAZAErFM";
-
-const supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
