99dfb9a9f90dae5a77aa4d090710237cbdfcbcde
AlexeyBoldin <aleksejboldin@gmail.com>
2026-07-25 04:28:46 +0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://ofwmifxoqpapabbygujo.supabase.co";
 
-const SUPABASE_KEY = "ВСТАВЬ_СЮДА_СВОЙ_ANON_KEY";
+const SUPABASE_KEY = "sb_publishable_TeE8cEYd4hTc6_NHU_luJw_oAZAErFM";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
